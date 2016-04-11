54d662ffcb1429ca8588795d19ce4b0c47d275f6
StanleyN7 <stanleycreamwave@gmail.com>
2016-04-11 12:48:17 +0300

    test

--- wp-content/themes/mytheme/index.php
+++ wp-content/themes/mytheme/index.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 <div class="main-content">
 	<?php if(have_posts()){
-		while(have_posts()){
-			the_post();?>
+		while(have_posts()){ ?>
+			<?php the_post();?>
 			<article class="block">
 				<h4><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
 				<a href="<?php the_permalink();?>" class="thumbnail"><?php the_post_thumbnail();?></a>
